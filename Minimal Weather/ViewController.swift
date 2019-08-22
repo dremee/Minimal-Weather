def632c4b487f44d4e8a45ea4f0d22fa4b0a74aa
Dremee <kirilllezhnin@mail.ru>
2019-08-22 11:58:57 +0500

    Initial Commit

---
+++ Minimal Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Minimal Weather
+//
+//  Created by Кирилл Лежнин on 8/22/19.
+//  Copyright © 2019 Кирилл Лежнин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
